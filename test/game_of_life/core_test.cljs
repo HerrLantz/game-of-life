0f29a2084fdbccb160b3e7c94470f30d22dba22f
Patric Lantz <patricla@kth.se>
2021-06-06 17:09:40 +0200

    Removed imports

--- test/game_of_life/core_test.cljs
+++ test/game_of_life/core_test.cljs
@@ -1,5 +1,4 @@
 (ns game-of-life.core-test
   (:require
-   [cljs.test :refer-macros [deftest is testing]]
-   [game-of-life.core :refer [multiply]]))
+   [cljs.test :refer-macros [deftest is testing]]))
 
