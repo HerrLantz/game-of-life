8eb7202b68a3b6e4e7b2717c499e63c2af374ddb
Patric Lantz <patricla@kth.se>
2021-06-06 16:34:02 +0200

    Template cleanup
    Removed all unnessesary coments/code from the figwheel-main template

--- src/game_of_life/core.cljs
+++ src/game_of_life/core.cljs
@@ -4,20 +4,15 @@
    [reagent.core :as reagent :refer [atom]]
    [reagent.dom :as rdom]))
 
-(println "This text is printed from src/game_of_life/core.cljs. Go ahead and edit it and see reloading in action.")
-
 (defn multiply [a b] (* a b))
 
-;; define your app data so that it doesn't get over-written on reload
 (defonce app-state (atom {:text "Hello world!"}))
 
 (defn get-app-element []
   (gdom/getElement "app"))
 
 (defn hello-world []
-  [:div
-   [:h1 (:text @app-state)]
-   [:h3 "Edit this in src/game_of_life/core.cljs and watch it change!"]])
+  [:h1 "Conway's game of life"])
 
 (defn mount [el]
   (rdom/render [hello-world] el))
@@ -26,14 +21,7 @@
   (when-let [el (get-app-element)]
     (mount el)))
 
-;; conditionally start your application based on the presence of an "app" element
-;; this is particularly helpful for testing this ns without launching the app
 (mount-app-element)
 
-;; specify reload hook with ^;after-load metadata
 (defn ^:after-load on-reload []
-  (mount-app-element)
-  ;; optionally touch your app-state to force rerendering depending on
-  ;; your application
-  ;; (swap! app-state update-in [:__figwheel_counter] inc)
-)
+  (mount-app-element))
