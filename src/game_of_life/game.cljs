222c77d6f9102f1c35f92e5acbade68919a1cc89
Patric Lantz <patricla@kth.se>
2021-06-06 17:14:31 +0200

    Added get-neighbours function
    Get the neighbours of any given cell

--- src/game_of_life/game.cljs
+++ src/game_of_life/game.cljs
@@ -5,3 +5,14 @@
 (defn create-cells
   []
   #{[0 0] [1 1] [1 0]})
+
+(defn get-neighbours
+  [[x y]]
+  #{[(inc x)      y]    ;; x+1 y
+    [x       (inc y)]   ;; x   y+1
+    [(inc x) (inc y)]   ;; x+1 y+1
+    [(dec x)      y]    ;; x-1 y
+    [x       (dec y)]   ;; x   y-1
+    [(dec x) (dec y)]   ;; x-1 y-1
+    [(dec x) (inc y)]   ;; x-1 y+1
+    [(inc x) (dec y)]})
