cb3c31d0ad5adea512154e061dd221d16060b918
Patric Lantz <patricla@kth.se>
2021-06-07 22:56:32 +0200

    Fixed bug
    Alive cells will now stay alive when two neighbours are alive.

--- src/game_of_life/game.cljs
+++ src/game_of_life/game.cljs
@@ -7,6 +7,12 @@
   []
   #{[0 0] [1 1] [1 0]})
 
+(defn toggle-cell
+  [alive-cells cell]
+  (if (contains? alive-cells cell)
+    (difference alive-cells #{cell})
+    (union alive-cells #{cell})))
+
 (defn get-neighbours
   [[x y]]
   #{[(inc x)      y]
@@ -21,18 +27,22 @@
 (defn get-all-cells
   [alive-cells]
   (-> (reduce (fn [acc curr] (-> (get-neighbours curr)
-                                 (union acc))) #{} alive-cells)))
+                                 (union acc))) alive-cells alive-cells)))
 
 (defn will-live?
-  [alive-cells cell]
-  (let [neighbours (get-neighbours cell)]
-    (-> neighbours
-        (intersection alive-cells)
-        count
-        (= 3))))
+  [alive-cells cell condition]
+  (as-> (get-neighbours cell) $
+        (intersection $ alive-cells)
+        (count $)
+        (condition $)))
+
+(defn filter-cells
+  [alive-cells cells condition]
+  (set (filter (fn [cell] (will-live? alive-cells cell condition)) cells)))
 
 (defn step
   [alive-cells]
-  (let [all-cells (get-all-cells alive-cells)]
-    (-> (filter (fn [cell] (will-live? alive-cells cell)) all-cells)
-        set)))
+  (let [all-cells (get-all-cells alive-cells)
+        dead-cells (difference all-cells alive-cells)]
+    (union (filter-cells alive-cells alive-cells (fn [c] (or (= c 2) (= c 3))))
+           (filter-cells alive-cells dead-cells  (fn [c] (= c 3))))))
