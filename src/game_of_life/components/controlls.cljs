72acdd7f9a1f1f363a93e17bec43ef192be15da4
Patric Lantz <patricla@kth.se>
2021-06-08 15:25:18 +0200

    Fixed indentation in maps

--- src/game_of_life/components/controlls.cljs
+++ src/game_of_life/components/controlls.cljs
@@ -1,12 +1,20 @@
 (ns game-of-life.components.controlls
   (:require [game-of-life.state :refer [dispatch]]))
 
+(defn get-style
+  []
+  {:border-style "none"
+   :padding "0.5rem 1rem"})
+
 (defn controlls
   []
   [:div
-   [:button {:on-click #(dispatch {:action :step})}
+   [:button {:style (get-style)
+             :on-click #(dispatch {:action :step})}
     "step"]
-   [:button {:on-click #(dispatch {:action :reset})}
+   [:button {:style (get-style)
+             :on-click #(dispatch {:action :reset})}
     "reset"]
-   [:button {:on-click #(dispatch {:action :clear})}
+   [:button {:style (get-style)
+             :on-click #(dispatch {:action :clear})}
     "clear"]])
