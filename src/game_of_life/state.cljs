7579f32874b5ad792b1e2e53c186a0fb1c28783e
Patric Lantz <patricla@kth.se>
2021-06-10 13:57:46 +0200

    Added save-action function

--- src/game_of_life/state.cljs
+++ src/game_of_life/state.cljs
@@ -4,17 +4,52 @@
                                        step]]
             [reagent.core      :as     r]))
 
-(defonce game-state-atom (r/atom (create-cells)))
+(def default-state
+  {:alive-cells (create-cells)
+   :history     '()
+   :actions     '()})
 
+(defonce game-state-atom (r/atom default-state))
+
+(defn- save-to-history
+  [game-state]
+  (if (= (:alive-cells game-state) (first (:history game-state)))
+    game-state
+    (update game-state :history conj (:alive-cells game-state))))
+
+(defn- get-previous-state
+  [game-state]
+  (-> (assoc game-state :alive-cells (first (:history game-state)))
+      (update :history rest)))
+
+(defn clear-cells
+  [game-state]
+  (assoc game-state :alive-cells #{}))
+
+(defn save-action
+  [game-state action]
+  (update game-state :actions conj action))
+
+(defn next-step
+  [game-state]
+  (-> (save-to-history game-state)
+      (update :alive-cells step)))
+
+(defn toggle
+  [game-state cell]
+  (update game-state :alive-cells (fn [gs] (toggle-cell gs cell))))
 
 (defn dispatch
   [{action :action value :value}]
+  (swap! game-state-atom save-action {:action action :value value})
   (condp = action
-    :step        (swap! game-state-atom step)
-    :toggle-cell (swap! game-state-atom toggle-cell value)
-    :reset       (reset! game-state-atom (create-cells))
-    :clear       (reset! game-state-atom #{})))
+    :step        (swap! game-state-atom next-step)
+    :back        (swap! game-state-atom get-previous-state)
+    :toggle-cell (swap! game-state-atom toggle value)
+    :reset       (reset! game-state-atom default-state)
+    :clear       (swap! game-state-atom clear-cells)))
 
 (defn get-cells
   []
-  (deref game-state-atom))
+  (:alive-cells (deref game-state-atom)))
+
